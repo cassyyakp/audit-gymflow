f78a42119c578283aab7906e1f33083d132b3bd2
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-07-22 08:11:12 +0000

    essaie de resolution du probleme cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,12 +15,13 @@ return [
 
     // Permet d'autoriser l'URL définie dans FRONTEND_URL ou toutes les sous-domaines Railway
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'https://*.railway.app',
+        'http://localhost:5173',
         'https://illustrious-integrity-production-e161.up.railway.app',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '^https://.*\.up\.railway\.app$',
+    ],
 
     'allowed_headers' => ['*'],
 
