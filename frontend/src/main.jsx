c3b272df5b24e241bf0d7cc91d953ec283ebfc15
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-07-23 10:12:41 +0000

    correction index.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,12 +8,8 @@ Sentry.init({
   dsn: "https://3e4af831654bdc91eb4aec8300e068c1@o4511704551784448.ingest.de.sentry.io/4511717152260176",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
   ],
   tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-
 });
 
 createRoot(document.getElementById('root')).render(
