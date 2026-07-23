a4097086fe4d26cf68897f7292a9925b95e6dbfa
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-07-23 10:46:42 +0000

    optimisation du main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,3 +17,20 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+if ('requestIdleCallback' in window) {
+  requestIdleCallback(initSentry)
+} else {
+  setTimeout(initSentry, 1000)
+}
+
+async function initSentry() {
+  const Sentry = await import('@sentry/react')
+  Sentry.init({
+    dsn: "https://3e4af831654bdc91eb4aec8300e068c1@o4511704551784448.ingest.de.sentry.io/4511717152260176",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+    ],
+    tracesSampleRate: 0.2, 
+  });
+}
